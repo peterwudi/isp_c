62bfec623cb0ef513c8fb8b384d4d22c76c43dce
peterwudi <peterwudi@gmail.com>
2013-11-18 16:54:47 -0500

    adding filter

--- bmpToRGB.c
+++ bmpToRGB.c
@@ -202,9 +202,9 @@ void bmpToRGB(char *inFilename, int proc)
 	//add50(image, infoheader.width, infoheader.height);
 	//emboss(image, infoheader.width, infoheader.height);
 	//takeleft(image, infoheader.width, infoheader.height);
-	//sharpen(image, infoheader.width, infoheader.height);
 	//bayer(image, infoheader.width, infoheader.height);
 	demosaic_neighbor(image, infoheader.width, infoheader.height);
+	sharpen(image, infoheader.width, infoheader.height);
 	//rgb2ycc(image, infoheader.width, infoheader.height);
 
 	buf = (char *)malloc(sizeof(char) * header.offset);
