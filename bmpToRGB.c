3a6842ca9ed3bbff8c5e28417e5917540e18d84c
peterwudi <peterwudi@gmail.com>
2013-11-19 12:17:50 -0500

    added rgb2ycc to the pipeline

--- bmpToRGB.c
+++ bmpToRGB.c
@@ -205,7 +205,7 @@ void bmpToRGB(char *inFilename, int proc)
 	//bayer(image, infoheader.width, infoheader.height);
 	demosaic_neighbor(image, infoheader.width, infoheader.height);
 	sharpen(image, infoheader.width, infoheader.height);
-	//rgb2ycc(image, infoheader.width, infoheader.height);
+	rgb2ycc(image, infoheader.width, infoheader.height);
 
 	buf = (char *)malloc(sizeof(char) * header.offset);
 	
